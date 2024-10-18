76bcde28c7882ee533bb91a6183b349d290868ac
Jorge1Rodriguez <164286636+Jorge1Rodriguez@users.noreply.github.com>
2024-10-18 09:53:41 -0500

    Delete app directory

--- app/src/main/java/com/unibague/prototipo_k9delivery/ui/theme/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.unibague.prototipo_k9delivery.ui.theme.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
